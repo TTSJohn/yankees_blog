ee11831bbf8e0ba2c19fb03e71e43f697b8752c9
John Moranski <johnnymoranski21@gmail.com>
2017-11-27 10:14:30 -0600

    john started working on this

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,39 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function (){
+
+    function initialize() {
+      var mapOptions = {
+      center: { lat: 40.8296, lng: -73.9262 },
+      zoom: 16
+      };
+        
+
+     var map = new google.maps.Map(document.getElementById('map-canvas'),
+                mapOptions);    
+        var image = "<%= asset_path 'yankee_stadium.jpeg'%>"
+      var marker = new google.maps.Marker({
+      position: { lat: 40.8296, lng: -73.9262 },
+      map: map,
+     title: 'Yankee Stadium',
+     icon: image,
+    });
+     var contentString = '<h2>Yankee Stadium</h2>' +
+        '<p>Home of the Greatest Franchise in Sports</p>';
+
+    var infowindow = new google.maps.InfoWindow({
+       content: contentString
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+     });
+    }
+      
+    google.maps.event.addDomListener(window, 'load', initialize);
+});
